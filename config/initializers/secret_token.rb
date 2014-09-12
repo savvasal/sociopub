643e94165ed3a405f1950eb7b6fb4efdae64a1b1
Savvas Alexandrou <salexa@localhost.localdomain>
2014-09-12 09:08:11 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sociopub::Application.config.secret_key_base = 'ef3c9d07979f63e2b7bcaaebdceee27bac84ac5d907c66f74c512c4659a2a45eac94f5050614468788a1f237ddb2563dfbc7c1a51e6f397981aa7a98fee82d76'

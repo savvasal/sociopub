9a459cd4945b57d34389417781724d787082a4db
Savinos90 <salexa03@cs.ucy.ac.cy>
2014-09-30 13:02:38 +0300

    make a basic user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sociopub::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.

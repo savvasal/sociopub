a4bc3abf45bc7ba65ac3871e54d14952d2bdde08
Savinos90 <salexa03@cs.ucy.ac.cy>
2014-10-01 14:17:00 +0300

    Make a basic User model (including secure passwords)

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,5 +1,8 @@
-base_title = "SocioPub"
+module CucumberRailsHelper
+end
+World(CucumberRailsHelper)
 
+base_title = "SocioPub"
 
 Given /^a user visits the 'Home Page'$/ do
   visit root_path
@@ -27,7 +30,7 @@ Given /^a user visits the 'Help Page'$/ do
 end
 
 Given /^a new user created$/ do
-  @user = User.new(name: "Example User", email: "user@example.com")
+  @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar" )
 end
 
 Then /^user should response to name$/ do
@@ -36,6 +39,22 @@ end
 
 And /^user should response to email$/ do
    expect(@user).to respond_to(:email)
+end    
+
+And /^user should response to password digest$/ do
+   expect(@user).to respond_to(:password_digest)
+end 
+
+And /^user should response to password$/ do
+   expect(@user).to respond_to(:password)
+end   
+ 
+And /^user should response to password confirmation$/ do
+   expect(@user).to respond_to(:password_confirmation)
+end    
+
+And /^user should response to authenticate$/ do
+   expect(@user).to respond_to(:authenticate)
 end    
 
 And /^user should be valid$/ do
@@ -57,8 +76,41 @@ Given /^a user with name (.*) and email like that (.*)$/ do |testname, testemail
   @user = User.new(name: testname , email: testemail)
 end
 
-Then /^it should not be valid$/ do
+Then /^user should not be valid$/ do
   expect(@user).not_to be_valid
 end
 
 
+Given /^an email address is already taken$/ do
+  @user = User.new(name: "Example User", email: "user@example.com")
+  user_with_same_email = @user.dup
+  user_with_same_email.email = @user.email.upcase
+  user_with_same_email.save
+end
+
+
+Given /^password is not present$/ do
+  @user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
+end
+
+Given /^password doesnt match confirmation$/ do
+  @user = User.new(name: "Example User", email: "user@example.com",password: "111", password_confirmation: "222")
+end
+
+Given /^a password is too short$/ do
+    @user = User.new(name: "Example User", email: "user@example.com",password: "a"*5, password_confirmation: "a"*5)
+end
+
+Given /^a user exist$/ do
+  @user = User.new(name: "username", email: "user@example.com",password: "password", password_confirmation: "password")
+  @user.save
+end
+
+And /^enter an invalid password$/ do
+  found_user = User.find_by(name: "username")
+  @invalid_user = found_user.authenticate("invalid")
+end
+
+Then /^password should not be valid$/ do
+  expect(@invalid_user).to be_false
+end
--- features/user.feature
+++ features/user.feature
@@ -4,19 +4,23 @@ Feature: User
     Given a new user created
     Then user should response to name
     And user should response to email
+    And user should response to password digest
+    And user should response to password
+    And user should response to password confirmation
+    And user should response to authenticate
     And user should be valid
 
   Scenario: Name not present
     Given a name is not present
-    Then it should not be valid
+    Then user should not be valid
 
   Scenario: Too long names
     Given a name is too long
-    Then it should not be valid
+    Then user should not be valid
 
   Scenario Outline: Ivalid email format
     Given a user with name <name> and email like that <email>
-    Then it should not be valid
+    Then user should not be valid
     Examples:
     | name | email             |
     | fooa | user@foo,com      |
@@ -24,3 +28,24 @@ Feature: User
     | fooc | example.user@foo. |
     | food | foo@bar_baz.com   |
     | fooe | foo@bar+baz.com   |
+ 
+  Scenario: Duplicate email
+    Given an email address is already taken
+    Then user should not be valid
+
+  Scenario: Password is not present
+    Given password is not present
+    Then user should not be valid
+
+  Scenario: Confirm password doesn't match
+    Given password doesnt match confirmation
+    Then user should not be valid
+
+  Scenario: Short password
+    Given a password is too short
+    Then user should not be valid
+
+  Scenario: Inalid password
+    Given a user exist
+    And enter an invalid password
+    Then password should not be valid

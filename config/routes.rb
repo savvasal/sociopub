f0d40cece2ffe66e0aa96afc61635a2ec34504a9
Savinos90 <salexa03@cs.ucy.ac.cy>
2014-10-02 09:28:28 +0300

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Sociopub::Application.routes.draw do
   resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
-  get "static_pages/help" 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

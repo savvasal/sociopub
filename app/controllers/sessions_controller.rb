851fc8da87a354b4a3cb5cdc3f769231a69fd7c1
Savinos90 <salexa03@cs.ucy.ac.cy>
2014-12-02 08:08:47 +0200

    finish login with username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
+# -*- coding: utf-8 -*-
 class SessionsController < ApplicationController
 
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = "άκυρο όνομα χρήστη ή κωδικός πρόσβασης"
       render 'new'
     end
   end

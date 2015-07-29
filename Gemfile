669eedd6337bbb450a999510e51f3fbcfeea688e
Savvas Alexandrou <salexa03@cs.ucy.ac.cy>
2015-07-29 10:11:30 +0300

    fix some db configurations

--- Gemfile
+++ Gemfile
@@ -48,6 +48,9 @@ gem 'simple-rss', '1.3.1'
 
 gem 'rails-i18n', '4.0.4'
 
+gem 'pg', '0.17.1'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', '3.4.0'
@@ -63,7 +66,6 @@ group :development, :test do
 end
 
 group :production do
-  gem 'pg', '0.17.1'
   gem 'rails_12factor', '0.0.2'
   gem 'puma', '2.11.1'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150728184504) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "feeds", force: :cascade do |t|
     t.string   "url"
     t.datetime "created_at", null: false
@@ -43,6 +46,6 @@ ActiveRecord::Schema.define(version: 20150728184504) do
     t.boolean  "admin",           default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end

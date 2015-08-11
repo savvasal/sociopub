33d0173cfd79df6567b76997b2b01b34b8f68d70
Savvas Alexandrou <salexa03@cs.ucy.ac.cy>
2015-08-11 18:10:49 +0300

    change environment configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'https://sociopub.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'box1048.bluehost.com',

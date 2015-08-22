f06cf09fd3b82ab8cf0158784590837da985e746
Savvas Alexandrou <salexa03@cs.ucy.ac.cy>
2015-08-22 14:17:58 +0300

    a change due to heroku deploy failure

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Sociopub
     config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end

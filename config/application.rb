eaba30e31743c187b7e1d1a9ac16ceb2e3eb432c
Savvas Alexandrou <salexa03@cs.ucy.ac.cy>
2015-08-22 13:53:15 +0300

    a change due to heroku deploy failure

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Sociopub
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.initialize_on_precompole = false
   end
 end

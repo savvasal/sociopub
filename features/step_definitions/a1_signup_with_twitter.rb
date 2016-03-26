0ea7609b57065885da4ae7e1f8c1c3ed98c35fd4
Savinos90 <salexa03@cs.ucy.ac.cy>
2016-03-26 19:02:40 +0200

    συγγραφή feature για εγγραφή με το λογαριασμό του twitter

---
+++ features/step_definitions/a1_signup_with_twitter.rb
@@ -0,0 +1,11 @@
+Δεδομένου(/^εμφανίζεται το εικονίδιο για εγγραφή μέσω twitter$/) do
+  page.should have_content("twitter icon")
+end
+
+Όταν(/^συνδέομαι μέσω του λογαριασμού μου στο twitter$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Τότε(/^να εγγράφομαι στην εφαρμογή$/) do
+  User.count.should == 1
+end
